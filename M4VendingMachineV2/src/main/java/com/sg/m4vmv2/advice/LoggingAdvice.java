9ad2199dd3657d47f22bdbfbd7963ecdf554bf5a
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-06-22 13:58:02 -0400

    Added AOP for exception throws

--- M4VendingMachineV2/src/main/java/com/sg/m4vmv2/advice/LoggingAdvice.java
+++ M4VendingMachineV2/src/main/java/com/sg/m4vmv2/advice/LoggingAdvice.java
@@ -5,25 +5,35 @@ import com.sg.m4vmv2.dao.VendingPersistenceException;
 import org.aspectj.lang.JoinPoint;
 
 public class LoggingAdvice {
+
     VMAuditDAO auditDAO;
 
     public LoggingAdvice(VMAuditDAO auditDAO) {
         this.auditDAO = auditDAO;
     }
-    
+
     public void createAuditEntry(JoinPoint jp) {
         Object[] args = jp.getArgs(); //gets the parameters
         String auditEntry = jp.getSignature().getName() + ": ";
-        
+
         for (Object currentArg : args) {
             auditEntry += currentArg;
         }
-        
+
         try {
             auditDAO.writeAuditEntry(auditEntry);
         } catch (VendingPersistenceException e) {
-            System.err.println(
-                    "ERROR: Could not create audit entry in LoggingAdvice.");
+            System.err.println("ERROR: Could not create audit entry in LoggingAdvice.");
+        }
+    }
+
+    public void createExceptionEntry(Exception ex) {
+        String auditEntry = "Thrown: " + ex.getMessage();
+
+        try {
+            auditDAO.writeAuditEntry(auditEntry);
+        } catch (VendingPersistenceException e) {
+            System.err.println("ERROR: could not write audit entry in LoggingAdvice.");
         }
     }
 }
