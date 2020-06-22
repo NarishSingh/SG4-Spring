8a3b7f66933eb4b5effd92c4c0790fd7927badcf
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-06-22 14:10:31 -0400

    Added AOP logging for exceptions thrown

--- M3ClassRosterV6AOP/src/main/java/com/sg/classroster/advice/LoggingAdvice.java
+++ M3ClassRosterV6AOP/src/main/java/com/sg/classroster/advice/LoggingAdvice.java
@@ -19,16 +19,25 @@ public class LoggingAdvice {
     public void createAuditEntry(JoinPoint jp) {
         Object[] args = jp.getArgs(); //gets the parameters
         String auditEntry = jp.getSignature().getName() + ": ";
-        
+
         for (Object currentArg : args) {
             auditEntry += currentArg;
         }
-        
+
         try {
             auditDAO.writeAuditEntry(auditEntry);
         } catch (ClassRosterPersistenceException e) {
-            System.err.println(
-                    "ERROR: Could not create audit entry in LoggingAdvice.");
+            System.err.println("ERROR: Could not create audit entry in LoggingAdvice.");
+        }
+    }
+
+    public void createExceptionEntry(Exception ex) {
+        String auditEntry = "Thrown: " + ex.getMessage();
+
+        try {
+            auditDAO.writeAuditEntry(auditEntry);
+        } catch (ClassRosterPersistenceException e) {
+            System.err.println("ERROR: could not write audit entry in LoggingAdvice.");
         }
     }
 }
