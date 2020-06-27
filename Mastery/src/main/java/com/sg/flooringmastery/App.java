2513d3f905c40b81ecbaf9e45aecdf25fe067a35
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-06-27 18:22:56 -0400

    Renaming

---
+++ Mastery/src/main/java/com/sg/flooringmastery/App.java
@@ -0,0 +1,14 @@
+/*
+M4 Master - TSG Flooring Corp
+Narish Singh
+Date Created: 6/23/20
+Last Modified:
+*/
+package com.sg.flooringmastery;
+
+public class App {
+    
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Mastery/src/main/java/com/sg/flooringmastery/view/FlView.java
@@ -0,0 +1,5 @@
+package com.sg.flooringmastery.view;
+
+public class FlView {
+    
+}
