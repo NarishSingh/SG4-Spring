67a5d558c6a3daf8909507852b0ca01e16e5d3dc
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-06-29 13:19:07 -0400

    Renaming Class Roster V6

---
+++ M4ClassRosterV6AOP/src/main/java/com/sg/classroster/advice/LoggingAdvice.java
@@ -0,0 +1,45 @@
+package com.sg.classroster.advice;
+
+import com.sg.classroster.dao.ClassRosterAuditDAO;
+import com.sg.classroster.dao.ClassRosterPersistenceException;
+import com.sg.classroster.service.ClassRosterDataValidationException;
+import com.sg.classroster.service.ClassRosterDuplicateIDException;
+import org.aspectj.lang.JoinPoint;
+
+/**
+ *
+ * @author naris
+ */
+public class LoggingAdvice {
+
+    ClassRosterAuditDAO auditDAO;
+
+    public LoggingAdvice(ClassRosterAuditDAO auditDAO) {
+        this.auditDAO = auditDAO;
+    }
+
+    public void createAuditEntry(JoinPoint jp) {
+        Object[] args = jp.getArgs(); //gets the parameters
+        String auditEntry = jp.getSignature().getName() + ": ";
+
+        for (Object currentArg : args) {
+            auditEntry += currentArg;
+        }
+
+        try {
+            auditDAO.writeAuditEntry(auditEntry);
+        } catch (ClassRosterPersistenceException e) {
+            System.err.println("ERROR: Could not create audit entry in LoggingAdvice.");
+        }
+    }
+
+    public void createExceptionEntry(Exception ex) {
+        String auditEntry = "Thrown: " + ex.getMessage();
+
+        try {
+            auditDAO.writeAuditEntry(auditEntry);
+        } catch (ClassRosterPersistenceException e) {
+            System.err.println("ERROR: could not write audit entry in LoggingAdvice.");
+        }
+    }
+}
