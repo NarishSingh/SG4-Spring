3423ce43d65bcfe15d1cd51611aab613d68e6daf
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-06-29 13:52:05 -0400

    Adding class work from 6-29

--- M4ClassRosterV6AOP/src/main/java/com/sg/classroster/advice/LoggingAdvice.java
+++ M4ClassRosterV6AOP/src/main/java/com/sg/classroster/advice/LoggingAdvice.java
@@ -2,8 +2,6 @@ package com.sg.classroster.advice;
 
 import com.sg.classroster.dao.ClassRosterAuditDAO;
 import com.sg.classroster.dao.ClassRosterPersistenceException;
-import com.sg.classroster.service.ClassRosterDataValidationException;
-import com.sg.classroster.service.ClassRosterDuplicateIDException;
 import org.aspectj.lang.JoinPoint;
 
 /**
@@ -35,6 +33,16 @@ public class LoggingAdvice {
 
     public void createExceptionEntry(Exception ex) {
         String auditEntry = "Thrown: " + ex.getMessage();
+
+        try {
+            auditDAO.writeAuditEntry(auditEntry);
+        } catch (ClassRosterPersistenceException e) {
+            System.err.println("ERROR: could not write audit entry in LoggingAdvice.");
+        }
+    }
+    
+    public void createExceptionWithJPEntry(JoinPoint jp, Exception ex) {
+        String auditEntry = "Exception Thrown in: " + jp.getSignature().getName() + ":: Message: " + ex.getMessage();
 
         try {
             auditDAO.writeAuditEntry(auditEntry);
