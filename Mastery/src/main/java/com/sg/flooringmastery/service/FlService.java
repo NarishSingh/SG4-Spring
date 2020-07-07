e5ba3182c4f2017d6bcef30b8186bd59eebfaac2
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-07-07 12:44:23 -0400

    Skeleton for service complete

--- Mastery/src/main/java/com/sg/flooringmastery/service/FlService.java
+++ Mastery/src/main/java/com/sg/flooringmastery/service/FlService.java
@@ -1,5 +1,94 @@
 package com.sg.flooringmastery.service;
 
-public class FlService {
-    
+import com.sg.flooringmastery.dao.OrderPersistenceException;
+import com.sg.flooringmastery.model.Order;
+import com.sg.flooringmastery.model.Product;
+import com.sg.flooringmastery.model.State;
+import java.time.LocalDate;
+import java.util.List;
+
+public interface FlService {
+
+    /**
+     * Validate an Order before proceeding with persisting an add or edit
+     *
+     * @param orderRequest {Order} a queued order
+     * @return {Order} a valid Order obj for persistance
+     * @throws InvalidProductException if an invalid product type is request
+     * @throws InvalidStateException   if an invalid state is requested
+     */
+    Order validateOrder(Order orderRequest) throws InvalidProductException,
+            InvalidStateException;
+
+    /**
+     * Add an active order to treemap and persist to file
+     *
+     * @param newOrder {Order} a validated new Order
+     * @return {Order} the successfully added order
+     * @throws OrderPersistenceException if cannot persist addition to file
+     */
+    Order addOrder(Order newOrder) throws OrderPersistenceException;
+
+    /**
+     * Remove an active order from treemap and persist to file
+     *
+     * @param date     {LocalDate} a valid date for an order
+     * @param orderNum {int} a valid order number
+     * @return {Order} the successfully removed order
+     * @throws OrderPersistenceException if cannot persist removal to file
+     */
+    Order removeOrder(LocalDate date, int orderNum) throws OrderPersistenceException;
+
+    /**
+     * Edit an active order in treemap and persist to file
+     *
+     * @param editedOrder {Order} a validated, edited Order obj
+     * @return {Order} the successfully edited order
+     * @throws OrderPersistenceException if cannot persist edit to file
+     */
+    Order editOrder(Order editedOrder) throws OrderPersistenceException;
+
+    /**
+     * Retrieve the info from a single active order
+     *
+     * @param date     {LocalDate} a valid date for an order
+     * @param orderNum {int} a valid number for an order
+     * @return {Order} the order retrieved
+     */
+    Order getOrder(LocalDate date, int orderNum);
+
+    /**
+     * Retrieve the info for all active orders on a given date
+     *
+     * @param date {LocalDate}a valid date for orders
+     * @return {List} all active orders on a given date
+     */
+    List<Order> getOrdersByDate(LocalDate date);
+
+    /**
+     * Export and persist all active orders to a single file
+     */
+    void exportOrder();
+
+    /**
+     * Retrieve all active Orders from file
+     *
+     * @return {List} a list of all orders
+     */
+    List<Order> getAllOrders();
+
+    /**
+     * Retrieve all valid states for business from file
+     *
+     * @return {List} a list of all state obj's
+     */
+    List<State> getValidStateList();
+
+    /**
+     * Retrieve all valid products for purchase from file
+     *
+     * @return {List} a list of all product obj's
+     */
+    List<Product> getValidProductList();
+
 }
---
+++ Mastery/src/main/java/com/sg/flooringmastery/service/FlServiceImpl.java
@@ -0,0 +1,130 @@
+package com.sg.flooringmastery.service;
+
+import com.sg.flooringmastery.dao.ExportFileDao;
+import com.sg.flooringmastery.dao.FlAuditDao;
+import com.sg.flooringmastery.dao.OrderDao;
+import com.sg.flooringmastery.dao.OrderPersistenceException;
+import com.sg.flooringmastery.dao.ProductDao;
+import com.sg.flooringmastery.dao.StateDao;
+import com.sg.flooringmastery.model.Order;
+import com.sg.flooringmastery.model.Product;
+import com.sg.flooringmastery.model.State;
+import java.time.LocalDate;
+import java.util.List;
+
+public class FlServiceImpl implements FlService {
+
+    StateDao state;
+    ProductDao product;
+    OrderDao dao;
+    FlAuditDao auditDao;
+    ExportFileDao export;
+
+    public FlServiceImpl(StateDao state, ProductDao product, OrderDao dao, FlAuditDao auditDao, ExportFileDao export) {
+        this.state = state;
+        this.product = product;
+        this.dao = dao;
+        this.auditDao = auditDao;
+        this.export = export;
+    }
+
+    @Override
+    public Order validateOrder(Order orderRequest) throws InvalidProductException, InvalidStateException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Order addOrder(Order newOrder) throws OrderPersistenceException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Order removeOrder(LocalDate date, int orderNum) throws OrderPersistenceException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Order editOrder(Order editedOrder) throws OrderPersistenceException {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public Order getOrder(LocalDate date, int orderNum) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Order> getOrdersByDate(LocalDate date) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public void exportOrder() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Order> getAllOrders() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<State> getValidStateList() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    @Override
+    public List<Product> getValidProductList() {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+
+    /*HELPER METHODS*/
+    /**
+     * Validate a date passed from view to ensure that it is a future date
+     *
+     * @return {LocalDate} a future date
+     */
+    private LocalDate validateOrderDate() {
+
+    }
+
+    /**
+     * Validate user's state selection and construct a new State obj from file
+     *
+     * @param userState {String} user's inputted state
+     * @return {State} a valid State object
+     */
+    private State validateState(String userState) {
+
+    }
+
+    /**
+     * Validate user's product selection and construct a new Product obj from
+     * file
+     *
+     * @param userProduct {String} user's inputted product type
+     * @return {Product} a valid Product object
+     */
+    private Product validateProduct(String userProduct) {
+
+    }
+
+    /**
+     * Calculate and set the costs for the remaining fields of a order obj
+     *
+     * @param orderRequest {Order} a valid but incomplete order obj request
+     */
+    private void calculateOrderCosts(Order orderRequest) {
+
+    }
+
+    /**
+     * Find the highest order number so far, used to set the next order
+     * request's number
+     *
+     * @return {int} a number > 0
+     */
+    private int getHighestOrderNumber() {
+
+    }
+}
