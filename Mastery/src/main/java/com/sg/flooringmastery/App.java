e13819979b05b9069f988348f28d7068429fd14c
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2020-07-13 12:58:41 -0400

    fixes to Spring wiring

--- Mastery/src/main/java/com/sg/flooringmastery/App.java
+++ Mastery/src/main/java/com/sg/flooringmastery/App.java
@@ -6,9 +6,17 @@ Last Modified:
 */
 package com.sg.flooringmastery;
 
+import com.sg.flooringmastery.controller.Controller;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 public class App {
     
     public static void main(String[] args) {
+        ApplicationContext actx = new ClassPathXmlApplicationContext("applicationContext.xml");
         
+        Controller c = actx.getBean("controller", Controller.class);
+        
+        c.run();
     }
 }
